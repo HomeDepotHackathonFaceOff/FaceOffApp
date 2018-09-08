f83c51934d676a907f29dbc2d2e87f72cebaa642
MHromiak <mhromiak3@gatech.edu>
2018-09-08 00:36:11 -0400

    Try to add camera support

--- app/src/main/java/com/example/asus/face_off/Intro.java
+++ app/src/main/java/com/example/asus/face_off/Intro.java
@@ -1,22 +1,39 @@
 package com.example.asus.face_off;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Intro extends AppCompatActivity {
 
+    private ArrayList<String> data = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
     }
 
     public void goToGamesScreen(View view) {
-        Intent toGames = new Intent(Intro.this, gamesScreen.class);
+        Intent toGames = new Intent(Intro.this, cameraFront.class);
         startActivity(toGames);
     }
+
+
 }
---
+++ app/src/main/java/com/example/asus/face_off/friendGames.java
@@ -0,0 +1,14 @@
+package com.example.asus.face_off;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class friendGames extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_games);
+    }
+
+}
