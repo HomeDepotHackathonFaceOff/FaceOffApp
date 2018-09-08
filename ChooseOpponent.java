c510d44551a0fcda929c8e1452ac71b528377a67
ruebensammiches <43081152+ruebensammiches@users.noreply.github.com>
2018-09-08 09:05:05 -0400

    Add files via upload

---
+++ ChooseOpponent.java
@@ -0,0 +1,13 @@
+package com.example.asus.face_off;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseOpponent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_opponent);
+    }
+}
