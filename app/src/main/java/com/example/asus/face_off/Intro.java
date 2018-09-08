68c876d889b8bc855ccef12696091615a9b1a5b8
MHromiak <mhromiak3@gatech.edu>
2018-09-07 20:40:54 -0400

    Commit new Project

--- app/src/main/java/com/example/asus/face_off/Intro.java
+++ app/src/main/java/com/example/asus/face_off/Intro.java
@@ -3,11 +3,11 @@ package com.example.asus.face_off;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class IntroScreen extends AppCompatActivity {
+public class Intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro_screen);
+        setContentView(R.layout.activity_intro);
     }
 }
