a0c83854ea39d0ba2e3aef96bff45440b03d15a5
MHromiak <mhromiak3@gatech.edu>
2018-09-07 22:27:19 -0400

    add gamesScreen activity

--- app/src/main/java/com/example/asus/face_off/Intro.java
+++ app/src/main/java/com/example/asus/face_off/Intro.java
@@ -1,7 +1,11 @@
 package com.example.asus.face_off;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Intro extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+    }
+
+    public void goToGamesScreen(View view) {
+        Intent toGames = new Intent(Intro.this, gamesScreen.class);
+        startActivity(toGames);
     }
 }
