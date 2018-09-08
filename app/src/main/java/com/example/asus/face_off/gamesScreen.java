b8394253ce5e249c3f8075e9abb1e7a41cc467fe
MHromiak <mhromiak3@gatech.edu>
2018-09-08 05:45:52 -0400

    Changed icon names to lowercase

--- app/src/main/java/com/example/asus/face_off/gamesScreen.java
+++ app/src/main/java/com/example/asus/face_off/gamesScreen.java
@@ -1,10 +1,12 @@
 package com.example.asus.face_off;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,4 +23,8 @@ public class gamesScreen extends AppCompatActivity {
         setContentView(R.layout.activity_games_screen);
     }
 
+    public void goToCamera(View view) {
+        Intent toCamera = new Intent(this, cameraFront.class);
+        startActivity(toCamera);
+    }
 }
